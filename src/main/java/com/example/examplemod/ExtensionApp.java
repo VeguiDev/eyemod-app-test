6ac92570cb88e5e5d109d6e8830fc362801ae452
VeguiDev <veguigameplays@gmail.com>
2023-03-08 02:08:50 -0300

    Added new widgets and panels:
    PANELS:
    
    - ErrorPanel
    
    WIDGETS:
    
    - ReasonInput
    - Call Button
    - Title Label

--- src/main/java/com/example/examplemod/ExtensionApp.java
+++ src/main/java/com/example/examplemod/ExtensionApp.java
@@ -4,54 +4,53 @@ import com.podloot.eyemod.gui.apps.App;
 import com.podloot.eyemod.lib.gui.util.Line;
 import com.podloot.eyemod.lib.gui.widgets.EyeButton;
 import com.podloot.eyemod.lib.gui.widgets.EyeLabel;
+import com.podloot.eyemod.lib.gui.widgets.EyeTextField;
 
 import net.minecraft.resources.ResourceLocation;
 
 public class ExtensionApp extends App {
 	
-	EyeLabel label;
-	EyeButton button;
-	Boolean textShowed = false;
-	
+	EyeButton callButton;
+	EyeLabel mainPanelTitle;
+	EyeLabel reasonInputLabel;
+	EyeTextField reasonInput;
+	ErrorPanel errorPanel = new ErrorPanel("Default Error");
+
 	public ExtensionApp() {
-		super(new ResourceLocation(ExampleMod.MODID, "textures/gui/apps/appicon.png"), 0xffCCCCCC, "Test App");
+		super(new ResourceLocation(ExampleMod.MODID, "textures/gui/apps/appicon.png"), 0xffCCCCCC, "Emergency");
 	}
-	
-	// This method toggles the visibility of the label and changes the text of the button.
-	void toggleText() {
-		
-		label.hide(textShowed);
-		
-		button.setText(new Line(textShowed?"Show Text":"Hide Text"));
-		
-		textShowed = !textShowed;
-		
-	}
-	
+
 	@Override
 	public boolean load() {
 		
-		// Initialize the text label
-		Line labelText = new Line("Example Text");
-		
-		label = new EyeLabel(70, 20, labelText);
-		
-		label.hide(!textShowed);
-		
-		add(label, 0, 60); // Adds its to the main app panel.
-		
-		// Initialize the button widget.
-		button = new EyeButton(60, 15, new Line("Show Text"));
-		
-		// Register action
-		button.setAction(() -> {
-			
-			this.toggleText();
-			
+		mainPanelTitle = new EyeLabel(100, 25, new Line("Call Police"));
+
+		reasonInputLabel = new EyeLabel(100, 25, new Line("Reason"));
+
+		reasonInput = new EyeTextField(100, 25);
+
+		callButton = new EyeButton(25, 25, new Line("CALL"));
+
+		callButton.setAction(() -> {
+			errorPanel.hide(true);
+			String reason = reasonInput.getInput();
+
+			if(reason.length() <= 0) {
+				errorPanel.setError("The reason input is required!");
+				errorPanel.hide(false);
+			} else {
+				System.out.println(reason);
+				reasonInput.setInput("");
+			}
+
 		});
-		
-		add(button, 0, 0); // Adds it to the main app panel.
-		
+
+		add(callButton, 65, 155);
+		add(mainPanelTitle, 25, 5);
+		add(reasonInput, 25, 125);
+		add(reasonInputLabel, 0, 100);
+		add(errorPanel, 0, 0);
+
 		return true;
 	}
 }
